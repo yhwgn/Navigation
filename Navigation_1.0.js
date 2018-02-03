4deffb759fd2df4dd75c0c630d3be997e4273916
yhwgn <35598901+yhwgn@users.noreply.github.com>
2018-02-03 23:13:05 +0900

    Create Navigation_1.0.js

---
+++ Navigation_1.0.js
@@ -0,0 +1,23 @@
+var pz, py, px;
+var z, y, x;
+var lz, ly, lx;
+var hptn;
+var quadrant;
+var angle;
+
+function newLevel(){
+    z = 0;
+    y = 0;
+    x = 0;
+}
+
+function modTick(){
+    pz = Player.getZ();
+    py = player.getY();
+    px = Player.getX();
+    lz = z-pz;
+    ly = y-py;
+    lx = x-px;
+    hptn = Math.sqrt(lz*lz+lx*lx);
+    clientMessage(Math.acos(((lz*lz)+(lx*lx)-(hptn*hptn))/(2*lz*lx)))
+}
