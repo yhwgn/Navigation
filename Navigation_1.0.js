5c52de85b4eaeb436b3105c3285b02ddeca9c0a7
yhwgn <35598901+yhwgn@users.noreply.github.com>
2018-02-04 15:49:21 +0900

    Update Navigation_1.0.js

--- Navigation_1.0.js
+++ Navigation_1.0.js
@@ -67,7 +67,7 @@ try{
 	var canvas = new Canvas(bitmap);
 	var paint = new Paint();
 	var i=0;
-	for(var x=0; x<16; X++){
+	for(var x=0; x<16; x++){
 		for(var y=0; y<16; y++){
 			if(sword[i] != 0){
 				var color = colorCode[sword[i]-1];
