6f21b9141dd03a7bbaccc5bb90a8229e0a852304
yhwgn <35598901+yhwgn@users.noreply.github.com>
2018-02-04 16:23:31 +0900

    Update Navigation_1.0.js

--- Navigation_1.0.js
+++ Navigation_1.0.js
@@ -139,8 +139,7 @@ function makeBtn(){
 			var btn = new Button(ctx);
 			var paintDrawable = new PaintDrawable(Color.rgb(themeColor.r, themeColor.g, themeColor.b));
             		paintDrawable.setCornerRadius(dp(15));
-			var dia = new android.graphics.drawable.BitmapDrawable(navigator);
-            		btn.setBackgroundDrawable(dia);
+            		btn.setBackgroundDrawable(paintDrawable);
 			btn.setText("NV");
 			btn.setPadding(dp(-5), dp(-5), dp(-5), dp(-5));
 			btn.setTextSize(android.util.TypedValue.COMPLEX_UNIT_DIP, 12);
