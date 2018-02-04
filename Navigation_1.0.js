ad6ded301b8cf0aee010d87b636d6942f6552a35
yhwgn <35598901+yhwgn@users.noreply.github.com>
2018-02-04 10:53:55 +0900

    Update Navigation_1.0.js

--- Navigation_1.0.js
+++ Navigation_1.0.js
@@ -67,8 +67,8 @@ try{
 	var canvas = new Canvas(bitmap);
 	var paint = new Paint();
 	var i=0;
-	for(var x=0; x<16; X++;){
-		for(var y=0; y<16; y++;){
+	for(var x=0; x<16; X++){
+		for(var y=0; y<16; y++){
 			var paint = new Paint();
 			paint.setColor(colorCode[i]);
 			canvas.drawPoint(x, y, paint);
