abf63ba0636183983fa36f1ed6b42b00bcca2d04
yhwgn <yhwgn.github.com>
2018-02-06 19:14:43 +0900

    m

--- Navigation_1.0.js
+++ Navigation_1.0.js
@@ -6,6 +6,7 @@ var Button = android.widget.Button;
 var EditText = android.widget.EditText;
 var TextView = android.widget.TextView;
 var CheckBox = android.widget.CheckBox;
+var ImageView = android.widget.ImageView
 var Toast = android.widget.Toast;
 
 var Dialog = android.app.Dialog;
@@ -13,13 +14,18 @@ var AlertDialog = android.app.AlertDialog;
 
 var View = android.view.View;
 var Gravity = android.view.Gravity;
+var RotateAnimation = android.view.animation.RotateAnimation;
 
 var Color = android.graphics.Color;
 var Bitmap = android.graphics.Bitmap;
 var Canvas = android.graphics.Canvas;
 var Paint = android.graphics.Paint;
+var Matrix = android.graphics.Matrix;
 var PaintDrawable = android.graphics.drawable.PaintDrawable;
 var ColorDrawable = android.graphics.drawable.ColorDrawable;
+
+var TextWatcher = android.text.TextWatcher
+var TextUtils = android.text.TextUtils;
 
 var DialogInterface = android.content.DialogInterface;
 
@@ -37,7 +43,10 @@ var bz, by, bx;
 var selectNumber = 0;
 var pointName = ["길찾기 종료"];
 var pointLoc = ["termination"];
-var navigater = Bitmap.createBitmap(16, 16, Bitmap.Config.ARGB_8888);
+var navigater = null;
+var diaSword = null;
+var goldSword = null;
+var isRun = false;
 var sword = [
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 1,
@@ -48,7 +57,7 @@ var sword = [
 	0, 0, 4, 4, 0, 0, 0, 1, 2, 3, 2, 1, 0, 0, 0, 0,
 	0, 0, 4, 6, 4, 0, 1, 2, 3, 2, 1, 0, 0, 0, 0, 0,
 	0, 0, 0, 4, 5, 4, 2, 3, 2, 1, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 4, 5, 4, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 4, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 4, 5, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 7, 9, 4, 5, 6, 4, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 7,10, 8, 0, 4, 4, 6, 4, 0, 0, 0, 0, 0, 0,
@@ -66,15 +75,30 @@ var colorCode = [Color.argb(0, 0, 0, 0),
 		 Color.argb(255, 73, 54, 21),
 		 Color.argb(255, 40, 30, 11),
 		 Color.argb(255, 104, 78, 30),
-		 Color.argb(255, 137, 103, 39)];
+		 Color.argb(255, 137, 103, 39),
 
-try{
-	var canvas = new Canvas(navigater);
+		 Color.rgb(0, 0, 0),
+		 Color.rgb(96, 96, 56),
+		 Color.rgb(234, 238, 87),
+		 Color.rgb(205, 208, 80),
+		 Color.rgb(74, 75, 51),
+		 Color.rgb(156, 158, 69),
+		 Color.rgb(124, 126, 62),
+		 Color.rgb(73, 54, 21),
+		 Color.rgb(40, 30, 11),
+		 Color.rgb(104, 78, 30),
+		 Color.rgb(137, 103, 39)];
+
+var diamond = 0;
+var gold =1;
+function drawSword(type){
+	var bitmap = Bitmap.createBitmap(16, 16, Bitmap.Config.ARGB_8888);
+	var canvas = new Canvas(bitmap);
 	var i=0;
 	for(var y=0; y<16; y++){
 		for(var x=0; x<16; x++){
 			if(sword[i] != 0){
-				var color = colorCode[sword[i]];
+				var color = colorCode[sword[i]+(type*11)];
 				var paint = new Paint();
 				paint.setColor(color);
 				canvas.drawPoint(x, y, paint);
@@ -82,11 +106,11 @@ try{
 			i++;
 		}
 	}
-	ts("로딩 완료");
-} catch(err) {
-	print(err);
+	return bitmap;
 }
 
+diaSword = Bitmap.createScaledBitmap(drawSword(diamond), 160, 160, false);
+goldSword = Bitmap.createScaledBitmap(drawSword(gold), 160, 160, false);
 
 function newLevel(){
 	bz = 0;
@@ -113,17 +137,28 @@ function leaveGame(){
 	});
 }
 
-/*function useItem(x, y, z){
-    bx = x;
-    by = y;
-    bz = z;
-}
-
 function modTick(){
-    var player = {z:getPlayerZ(), x:getPlayerX()};
-    var point = {z:bz, x:bx};
-    clientMessage(getAngle(player, point)+"");
-}*/
+	if(isRun){
+		var player = {z:getPlayerZ(), x:getPlayerX()};
+    	var point = {z:bz, x:bx};
+		clientMessage(getAngle(player, point) + " : " + getYaw());
+		ui(function(){
+			try{
+				var matrix = new Matrix();
+				navigater.setScaleType(ImageView.ScaleType.MATRIX);   //required
+				matrix.postRotate(getAngle(player, point), 80, 80);
+				navigater.setImageMatrix(matrix);
+				/*var angle = getAngle(player, point);
+				var an = new RotateAnimation(0, angle, 25, 25);
+				an.setDuration(0);
+				an.setFillAfter(true);
+				navigater.setAnimation(an);*/
+			}catch(err){
+				print(err);
+			}
+		});
+	}
+}
 
 function getAngle(player, point){
 	var z = point.z-player.z;
@@ -135,23 +170,17 @@ function getAngle(player, point){
 	} else {
 		angle = Math.PI+angle;
 	}
-	return angle;
+	return (angle*(180/Math.PI));
 }
 
 function makeBtn(){
 	ui(function(){
 		try{
-			var btn = new Button(ctx);
-			var paintDrawable = new PaintDrawable(Color.rgb(themeColor.r, themeColor.g, themeColor.b));
-			paintDrawable.setCornerRadius(dp(15));
-			btn.setBackgroundDrawable(paintDrawable);
-			btn.setText("NV");
-			btn.setPadding(dp(-5), dp(-5), dp(-5), dp(-5));
-			btn.setTextSize(android.util.TypedValue.COMPLEX_UNIT_DIP, 12);
-			btn.setTextColor(Color.WHITE);
+			navigater = new ImageView(ctx);
+			navigater.setImageBitmap(goldSword);
 			var viewX,viewY,x,y,xx,yy;
 			var click = true;
-			btn.setOnTouchListener(new android.view.View.OnTouchListener(){
+			navigater.setOnTouchListener(new android.view.View.OnTouchListener(){
 				onTouch: function(v, event) {
 					switch(event.action) {
 						case android.view.MotionEvent.ACTION_DOWN:
@@ -164,7 +193,7 @@ function makeBtn(){
 							x = event.getRawX() - viewX;
 							y = event.getRawY() - viewY;
 							if(Math.abs(x-xx) > 100 || Math.abs(y-yy) > 100) click = false;
-							if(!click) btnWindow.update(x,y,dp(40),dp(30),true);
+							if(!click) btnWindow.update(x,y,dp(50),dp(50),true);
 							break;
 						case android.view.MotionEvent.ACTION_UP:
 							if(click) {
@@ -180,7 +209,7 @@ function makeBtn(){
 					return true;
 				}
 			});
-			btnWindow = new PopupWindow(btn, dp(40), dp(30));
+			btnWindow = new PopupWindow(navigater, dp(50), dp(50));
 			btnWindow.showAtLocation(ctx.getWindow().getDecorView(),Gravity.LEFT|Gravity.TOP,NV1_x,NV1_y);
 		} catch(err) {
 			print("load btn " + err.lineNumber + "\n" + err);
@@ -196,8 +225,19 @@ function openList(){
 			builder.setSingleChoiceItems(pointName, selectNumber, new DialogInterface.OnClickListener(){
 				onClick: function(d, i){
 					selectNumber = i;
-					if(i==0) d.getButton(AlertDialog.BUTTON_NEGATIVE).setEnabled(false);
-					else d.getButton(AlertDialog.BUTTON_NEGATIVE).setEnabled(true);
+					if(i==0){
+						d.getButton(AlertDialog.BUTTON_NEGATIVE).setEnabled(false);
+						navigater.setImageBitmap(goldSword);
+						isRun = false;
+					}else{
+						d.getButton(AlertDialog.BUTTON_NEGATIVE).setEnabled(true);
+						navigater.setImageBitmap(diaSword)
+						isRun = true;
+						var point = pointLoc[i].split(",");
+						bx = point[0];
+						by = point[1];
+						bz = point[2];
+					}
 				}
 			});
 			builder.setNegativeButton("삭제", null);
@@ -207,23 +247,30 @@ function openList(){
 					var name = new EditText(ctx);
 					var add = new AlertDialog.Builder(ctx);
 					add.setView(name);
+					add.setTitle("목적지 이름");
 					add.setNegativeButton("취소", null);
 					add.setPositiveButton("저장", new DialogInterface.OnClickListener(){
 						onClick: function(d){
 							try{
-								if(!(name.getText().toString().equals(""))){
-									pointName.push(name.getText().toString());
-									pointLoc.push(getPlayerX() + ":" + getPlayerY() + ":" + getPlayerZ());
-								} else {
-									ts("목적지 이름을 작성해 주십시오.");
-									add.show();
-								}
+								pointName.push(name.getText().toString());
+								pointLoc.push(getPlayerX() + "," + getPlayerY() + "," + getPlayerZ());
 							}catch(err){
 								print(err.lineNumber + "\n" + err);
 							}
 						}
 					});
-					add.show();
+					var addDialog = add.create();
+					addDialog.show();
+					var save = addDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+					save.setEnabled(false);
+					name.addTextChangedListener(new TextWatcher(){
+						onTextChanged: function(s, start, before, count){},
+						beforeTextChanged: function(s, start, count, after){},
+						afterTextChanged: function(s){
+							if(name.getText().toString().equals("") || name.getText().toString().indexOf(":")!=-1 || name.getText().toString().indexOf(",")!=-1) save.setEnabled(false);
+							else save.setEnabled(true);
+						}
+					});
 				}
 			});
 			var dialog = builder.create();
